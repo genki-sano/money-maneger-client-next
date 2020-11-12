25fae4db7c023add2ae85b3edf2207394ea19c63
g-sano <g.sano.2441@gmail.com>
2020-11-12 23:08:32 +0900

    型パズル

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,21 +1,22 @@
-import { render } from '@testing-library/react'
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
+import React from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+// import { ThemeProvider } from 'my-ui-lib'
+// import { TranslationProvider } from 'my-i18n-lib'
+// import defaultStrings from 'i18n/en-x-default'
 
-const Providers = ({ children }) => {
-  return children
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
-}
+// const Providers: React.FC = ({ children }) => {
+// return (
+//   <ThemeProvider theme="light">
+//     <TranslationProvider messages={defaultStrings}>
+//       {children}
+//     </TranslationProvider>
+//   </ThemeProvider>
+// )
+// }
 
-const customRender = (ui, options = {}) =>
-  render(ui, { wrapper: Providers, ...options })
+const customRender = (ui: React.ReactElement, options: RenderOptions = {}) =>
+  render(ui, { ...options })
+// render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
